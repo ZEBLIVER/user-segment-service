ec1e222031eacf9133299109f57d5e396a4561f5
Grigoriy <quracau@gmail.com>
2025-10-01 19:52:57 +0300

    инициализация проекта

---
+++ src/main/java/com/study/segment_service/SegmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.study.segment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegmentServiceApplication.class, args);
+	}
+
+}
