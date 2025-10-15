13d692b26b80d6d2cdde2c46bc11d807371982d5
Grigoriy <quracau@gmail.com>
2025-10-15 15:16:00 +0300

    docs(swagger): add API documentation for controllers

--- src/main/java/com/study/segment_service/controller/SegmentController.java
+++ src/main/java/com/study/segment_service/controller/SegmentController.java
@@ -3,11 +3,17 @@ package com.study.segment_service.controller;
 import com.study.segment_service.exeption.ResourceNotFoundException;
 import com.study.segment_service.model.Segment;
 import com.study.segment_service.service.SegmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "Segment Management",
+        description = "Операции CRUD для сегментов и управление распределением пользователей")
 @RestController
 @RequestMapping("/api/segments")
 public class SegmentController {
@@ -18,6 +24,11 @@ public class SegmentController {
         this.segmentService = segmentService;
     }
 
+    @Operation(summary = "Создать новый сегмент", description = "Создает сегмент с уникальным именем.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Сегмент успешно создан"),
+            @ApiResponse(responseCode = "409", description = "Сегмент с таким именем уже существует (Конфликт)")
+    })
     @PostMapping()
     public ResponseEntity<Segment> createSegment(@RequestBody Segment segment) {
         try {
@@ -28,6 +39,11 @@ public class SegmentController {
         }
     }
 
+    @Operation(summary = "Удалить сегмент", description = "Удаляет сегмент по его имени, открепляя его от всех пользователей.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Сегмент успешно удален (Нет содержимого)"),
+            @ApiResponse(responseCode = "404", description = "Сегмент не найден")
+    })
     @DeleteMapping("/{segmentName}")
     public ResponseEntity<Void> deleteSegment(@PathVariable String segmentName) {
         try {
@@ -38,6 +54,11 @@ public class SegmentController {
         }
     }
 
+    @Operation(summary = "Получить сегмент по ID", description = "Возвращает информацию о сегменте по его уникальному ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Сегмент успешно найден и возвращен"),
+            @ApiResponse(responseCode = "404", description = "Сегмент с указанным ID не найден")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Segment> getSegmentById(@PathVariable Long id) {
         try {
@@ -48,6 +69,12 @@ public class SegmentController {
         }
     }
 
+    @Operation(summary = "Переименовать сегмент", description = "Переименовывает существующий сегмент.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Сегмент успешно переименован (Нет содержимого)"),
+            @ApiResponse(responseCode = "404", description = "Исходный сегмент не найден"),
+            @ApiResponse(responseCode = "409", description = "Сегмент с новым именем уже существует (Конфликт)")
+    })
     @PutMapping("/rename/{segmentName}")
     public ResponseEntity<Void> renameSegment(@PathVariable String segmentName,
                                               @RequestBody String newSegmentName) {
@@ -61,6 +88,11 @@ public class SegmentController {
         }
     }
 
+    @Operation(summary = "Добавить пользователя в сегмент", description = "Прикрепляет пользователя к указанному сегменту.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Пользователь успешно добавлен (Нет содержимого)"),
+            @ApiResponse(responseCode = "404", description = "Сегмент или Пользователь не найден")
+    })
     @PutMapping("/{segmentName}/user/{userId}")
     public ResponseEntity<Void> addUserToSegment(@PathVariable String segmentName,
                                                  @PathVariable Long userId) {
@@ -72,6 +104,11 @@ public class SegmentController {
         }
     }
 
+    @Operation(summary = "Удалить пользователя из сегмента", description = "Открепляет пользователя от указанного сегмента.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Пользователь успешно удален из сегмента (Нет содержимого)"),
+            @ApiResponse(responseCode = "404", description = "Сегмент или Пользователь не найден")
+    })
     @DeleteMapping("/{segmentName}/user/{userId}")
     public ResponseEntity<Void> deleteUserFromSegment(@PathVariable String segmentName,
                                                       @PathVariable Long userId) {
@@ -84,8 +121,14 @@ public class SegmentController {
 
     }
 
+    @Operation(summary = "Распределить пользователей по сегменту", description = "Распределяет указанный процент всех существующих пользователей в данный сегмент.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Распределение завершено (Нет содержимого)"),
+            @ApiResponse(responseCode = "404", description = "Сегмент не найден"),
+            @ApiResponse(responseCode = "400", description = "Неверное значение процента (должно быть в диапазоне (0, 100])")
+    })
     @PostMapping("/distribute/{segmentName}")
-    public ResponseEntity<VerifyError> distributeUsersToSegment(
+    public ResponseEntity<Void> distributeUsersToSegment(
             @PathVariable String segmentName,
             @RequestBody Double percent) {
         try {
--- src/main/java/com/study/segment_service/controller/UserController.java
+++ src/main/java/com/study/segment_service/controller/UserController.java
@@ -3,6 +3,10 @@ package com.study.segment_service.controller;
 import com.study.segment_service.exeption.ResourceNotFoundException;
 import com.study.segment_service.model.User;
 import com.study.segment_service.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Set;
 
+@Tag(name="User Management")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -21,17 +26,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Создать нового пользователя")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Пользователь успешно создан"),
+            @ApiResponse(responseCode = "400", description = "Неверный формат")
+    })
     @PostMapping()
     public ResponseEntity<User> createUser(@RequestBody User user) {
         User savedUser = userService.createUser(user);
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Получить всех пользователей")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список пользователей успешно получен")
+    })
     @GetMapping("/all")
     public List<User> getAllUsers() {
         return userService.findAllUsers();
     }
 
+    @Operation(summary = "Получить все сегменты пользователя по его ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Список сегментов успешно возвращен"),
+            @ApiResponse(responseCode = "404", description = "Пользователь с указанным ID не найден")
+    })
     @GetMapping("/{userId}/segments")
     public ResponseEntity<Set<String>> getAllSegmentsByUserID(@PathVariable Long userId) {
         try {
